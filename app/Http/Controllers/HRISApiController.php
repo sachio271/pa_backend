f0917495f75ccdf44a1b72468a9543d551548cbf
Jessica Clarensia S <113441688+tootzie@users.noreply.github.com>
2024-10-01 11:19:28 +0700

    adding get_users

--- app/Http/Controllers/HRISApiController.php
+++ app/Http/Controllers/HRISApiController.php
@@ -104,4 +104,15 @@ class HRISApiController extends Controller
         );
         return response()->json(["message" => "sukses", 'data' => $results]);
     }
+
+    public function get_users() {
+        $results = DB::select('select  c.ektp,c.name
+            from masterstruct a
+            left join employeestruct b on a.id=b.struct
+            left join masteremployee c on c.ektp=b.ektp
+            left join employeestruct d on a.pastruct1=d.struct
+            left join masteremployee f on f.ektp=d.ektp');
+
+            return response()->json(["message" => "sukses", "data" => $results]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,4 @@ Route::get('/get_subordinates/{ektp}', [HRISApiController::class, 'get_subordina
 Route::get('/get_all_company', [HRISApiController::class, 'get_all_company'])->name('get_all_company');
 Route::get('/get_departments/{companyCode}', [HRISApiController::class, 'get_departments'])->name('get_departments');
 Route::get('/get_departments/{companyCode}/{department}', [HRISApiController::class, 'get_employee_department'])->name('get_employee_department');
+Route::get('/get_users', [HRISApiController::class, 'get_users'])->name('get_users');
