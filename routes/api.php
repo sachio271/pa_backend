1dd666dfbe23d7975afca9c5de0ea48a584eadf8
Jessica Clarensia S <113441688+tootzie@users.noreply.github.com>
2024-10-15 13:46:53 +0700

    fixing route name

--- routes/api.php
+++ routes/api.php
@@ -11,5 +11,5 @@ Route::get('/user', function (Request $request) {
 Route::get('/get_subordinates/{ektp}/{limit_date}', [HRISApiController::class, 'get_subordinates'])->name('get_subordinates');
 Route::get('/get_all_company', [HRISApiController::class, 'get_all_company'])->name('get_all_company');
 Route::get('/get_departments/{companyCode}', [HRISApiController::class, 'get_departments'])->name('get_departments');
-Route::get('/get_departments/{companyCode}/{department}', [HRISApiController::class, 'get_employee_department'])->name('get_employee_department');
+Route::get('/get_employee_department/{companyCode}/{department}', [HRISApiController::class, 'get_employee_department'])->name('get_employee_department');
 Route::get('/get_users', [HRISApiController::class, 'get_users'])->name('get_users');
